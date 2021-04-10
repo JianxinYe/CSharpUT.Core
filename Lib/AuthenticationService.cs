098a7fddee83b6ebc4141a48ecd8ad5b400c2fe1
jianxin <k082474@gmail.com>
2021-04-10 14:39:33 +0800

    Authentication

--- Lib/AuthenticationService.cs
+++ Lib/AuthenticationService.cs
@@ -5,15 +5,29 @@ namespace Lib
 {
     public class AuthenticationService
     {
+        private IProfile _profile;
+        private IToken _token;
+
+        public AuthenticationService(IProfile profile, IToken token)
+        {
+            _profile = profile;
+            _token = token;
+        }
+
+        public AuthenticationService()
+        {
+            _profile = new ProfileDao();
+            _token = new RsaTokenDao();
+        }
         public bool IsValid(string account, string password)
         {
             // 根據 account 取得自訂密碼
-            var profileDao = new ProfileDao();
-            var passwordFromDao = profileDao.GetPassword(account);
+            //var profileDao = new ProfileDao();
+            var passwordFromDao = _profile.GetPassword(account);
 
             // 根據 account 取得 RSA token 目前的亂數
-            var rsaToken = new RsaTokenDao();
-            var randomCode = rsaToken.GetRandom(account);
+            //var rsaToken = new RsaTokenDao();
+            var randomCode = _token.GetRandom(account);
 
             // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
             var validPassword = passwordFromDao + randomCode;
@@ -30,7 +44,13 @@ namespace Lib
         }
     }
 
-    public class ProfileDao
+    public interface IProfile
+    {
+        string GetPassword(string account);
+    }
+    
+
+    public class ProfileDao : IProfile
     {
         public string GetPassword(string account)
         {
@@ -55,11 +75,16 @@ namespace Lib
         }
     }
 
-    public class RsaTokenDao
+    public interface IToken
+    {
+        string GetRandom(string account);
+    }
+
+    public class RsaTokenDao : IToken
     {
         public string GetRandom(string account)
         {
-            var seed = new Random((int) DateTime.Now.Ticks & 0x0000FFFF);
+            var seed = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
             var result = seed.Next(0, 999999).ToString("000000");
             Console.WriteLine("randomCode:{0}", result);
 
